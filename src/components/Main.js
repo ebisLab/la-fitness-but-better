62df0dedbf786fffff1b95abe430ab6a9feb20be
ebisLab <ebaiden@techie.com>
2022-06-20 13:08:15 -0400

    update database list

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,40 @@
 import React, {useEffect, useState} from 'react';
 import {v4 as uuidv4} from 'uuid';
 import {mock} from '../api/mock';
-import axios from 'axios'
+// import axios from 'axios'
 
 export default function Main() {
     const [userBarcode, setUserBarcode]=useState('')
     const [userInfo, setUserInfo]=useState('')
     const [todaysList, setTodaysList]=useState([])
     const [currentUser, setCurrentUser]=useState([])
+    const [usersDatabase, setUsersDatabase]= useState([])
     const time= new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }) 
     const [currentTime,setCurrentTime]=useState(time)
 
     useEffect(()=>{
-      axios.get('')
+      setUsersDatabase(mock)
 
     },[])
     
     const addName =(name)=> {
-      let findUser= mock.some(item=>item.first_name.toLowerCase() === name.toLowerCase())
-      let filterUser = mock.filter(item=>item.first_name.toLowerCase() === name.toLowerCase())
+      let findUser= usersDatabase.some(item=>item.first_name.toLowerCase() === name.toLowerCase())
+      let filterUser = usersDatabase.filter(item=>item.first_name.toLowerCase() === name.toLowerCase())
       setCurrentUser(filterUser)
       if (findUser){
         const user={...filterUser[0], time, timesheet:[Date.now()]}
       setTodaysList([user,...todaysList]);
+
+      
+      //find the user from the database list and update their info
+      const db= usersDatabase.map(obj=> {
+        if(obj.barcode_number ===filterUser[0].barcode_number)
+        {return {...obj, time}}
+        else {
+          return obj
+        }
+      })
+      setUsersDatabase(db)
       }else{
       const user = { id: todaysList.length+1, first_name: name , status: 'UNRECOGNIZED', time};
       setTodaysList([ user,...todaysList]); 
@@ -41,6 +53,16 @@ export default function Main() {
     const kidsModal=()=>{
         console.log('heee')
     }
+
+    const changecurrentuser=()=>{
+      console.log('current user', currentUser[0].barcode_number)
+      let list = usersDatabase.filter(item=>item.barcode_number.toLowerCase() === currentUser[0].barcode_number.toLowerCase())
+      console.log('compare', list)
+    }
+
+    console.log('database list', usersDatabase)
+
+
   return (
   <main className='grid-container'>
     <section className='first-column'>
@@ -52,50 +74,6 @@ export default function Main() {
         </form>
 
       </div>
-      {/* <div>
-      <div className='member-card'>
-        <div className='member-info'>
-          <div style={{background: 'green'}}>status</div>
-          <div className='status'>
-            <table>
-              <tr>
-                <th>Barcode</th>
-                <td>{userInfo}</td>
-              </tr>
-              <tr>
-                <th>Name</th>
-                <td>member name</td>
-              </tr>
-              <tr>
-                <th>Expiration</th>
-                <td>none</td>
-              </tr>
-            </table>
-            <div>
-              <button>service</button>
-              <button>Get $</button>
-            </div>
-          </div>
-        </div>
-      <div className='member-privileges'>
-        <div>🎾</div>
-        <div>G(1)</div>
-        <button>Guest Waiver</button>
-      </div>
-      <div className='card-profile-picture'>
-        
-        <div style={{ height: "200px", width:'200px', background: "red"}} />
-        <div>
-          <button>Delete</button><button>Photo</button>
-        </div>
-      </div>
-      <div>
-        members under plan
-        <br/>
-        {userInfo}
-      </div>
-      </div>
-      </div> */}
       {currentUser.length? (currentUser.map((item)=>(
         <div key={Date.now()}>
         <div className='member-card'>
@@ -122,19 +100,19 @@ export default function Main() {
               </div>
             </div>
           </div>
-        <div className='member-privileges'>
-          <div>🎾</div>
-          <div>G(1)</div>
-          <button>Guest Waiver</button>
-        </div>
-        <div className='card-profile-picture'>
-        <div>
-        <div style={{ height: "200px", width:'200px', background: "red"}} />
-        <div>
-          <button>Delete</button><button>Photo</button>
-        </div> 
-                </div>
-      </div>
+          <div className='member-privileges'>
+            <div>🎾</div>
+            <div>G(1)</div>
+            <button>Guest Waiver</button>
+          </div>
+          <div className='card-profile-picture'>
+            <div>
+              <div style={{ height: "200px", width:'200px', background: "red"}} />
+              <div>
+                <button>Delete</button><button onClick={changecurrentuser}>Photo</button>
+              </div> 
+            </div>
+          </div>
         <div>
           members under plan
           <br/>
